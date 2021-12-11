0b6e7acc3dc5cf6971b97c0d6a9811d705460697
jmpfbmx <jmpf.bmx@gmail.com>
2021-12-12 00:22:03 +0100

    bouncycastle: Import MT6750/MT6755 ALPS Changes
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I4e42036ffc22f98fbbe200ec12306dd6ab866fca

--- Android.mk
+++ Android.mk
@@ -1,4 +1,9 @@
 #
+# Copyright (C) 2014 MediaTek Inc.
+# Modification based on code covered by the mentioned copyright
+# and/or permission notice(s).
+#
+#
 # Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -64,6 +69,20 @@ ifneq ($(TARGET_BUILD_PDK),true)
     LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
     LOCAL_JAVA_LANGUAGE_VERSION := 1.7
     include $(BUILD_JAVA_LIBRARY)
+
+    ifeq ($(strip $(BUILD_MTK_API_DEP)), yes)
+        # bouncycastle API table.
+        # ============================================================
+        LOCAL_MODULE := bouncycastle-api
+        LOCAL_SRC_FILES := $(android_bcprov_src_files)
+        LOCAL_JAVA_LIBRARIES += $(LOCAL_STATIC_JAVA_LIBRARIES)
+        LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+        LOCAL_DROIDDOC_OPTIONS:= \
+            -api $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/bouncycastle-api.txt \
+            -nodocs \
+            -hidden
+        include $(BUILD_DROIDDOC)
+    endif
 
     # unbundled bouncycastle jar
     include $(CLEAR_VARS)
